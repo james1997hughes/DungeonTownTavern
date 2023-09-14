f4fa79d9fcdac00597f952ff67b24a923e509ae5
James Hughes <james1997hughes@gmail.com>
2023-09-14 17:52:52 +0100

    Fix movement
    And 2d package - unity changed versioning?? Not sure

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   public float moveSpeed;
+    public float moveSpeed;
 
+    private bool isMoving;
 
-   private bool isMoving;
+    private Vector2 input;
 
-   private Vector2 input;
-   
-   private Animator animator;
+    private Animator animator;
 
-   public LayerMask solidObjectsLayer;
-   public LayerMask interactableLayer;
+    public LayerMask solidObjectsLayer;
 
-   private void Awake()
-   {
-    animator = GetComponent<Animator>();
-   }
+    public LayerMask interactableLayer;
 
-   public void HandleUpdate()
-   {
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // James: How is this being called? Why not use the standard unity Update?
+    public void HandleUpdate()
+    {
+       //  Debug.Log(isMoving);
         if (!isMoving)
-        { 
+        {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-           // Debug.Log("this is input.x" + input.x);
-           // Debug.Log("this is input.y" + input.y);
+            // Debug.Log("this is input.x" + input.x);
+            // Debug.Log("this is input.y" + input.y);
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
 
-            if (input.x !=0) input.y = 0;
-
-            
             if (input != Vector2.zero)
             {
-
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
 
@@ -44,53 +46,64 @@ public class PlayerController : MonoBehaviour
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
-                if (IsWalkable(targetPos))
+                if (IsWalkable(targetPos)){
                     StartCoroutine(Move(targetPos));
+                } else {
+                    Debug.Log("Not walkable!");
+                }
             }
         }
 
         animator.SetBool("isMoving", isMoving);
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z)){
             Interact();
-   }
+        }
+    }
 
     void Interact()
     {
-            var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
-            var interactPos = transform.position + facingDir;
+        var facingDir =
+            new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
 
-
-            // Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
-
-            var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
-            if (collider != null)
-            {
-                collider.GetComponent<Interactable>()?.Interact();
-            }
+        // Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
+        if (collider != null)
+        {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
     }
-
 
     IEnumerator Move(Vector3 targetPos)
-   {
-    isMoving = true;
-
-    while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        
+        // Identical behaviour as >Epsilon but 1. Reduces bouncing and 2. Doesn't randomly break for some fucking reason
+        while ((targetPos - transform.position).sqrMagnitude > 0.005f)//Mathf.Epsilon)
+        {
+            Debug.Log(targetPos);
+            Debug.Log(transform.position);
+            isMoving = true;
+            transform.position =
+                Vector3
+                    .MoveTowards(transform.position,
+                    targetPos,
+                    moveSpeed * Time.deltaTime);
             yield return null;
-    }
-    transform.position = targetPos;
+        }
+        transform.position = targetPos;
+        Debug.Log((targetPos - transform.position).sqrMagnitude);
 
-    isMoving = false;
-   }
-   
-    private bool IsWalkable(Vector3 targetPos)
-   {
-    if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null)
-    {
-        return false;
+        isMoving = false;
+        
     }
-    return true;
-   }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null)
+        {
+            return false;
+        }
+        return true;
+    }
 }
